5291b7f7f51b4b98d9f5d92fcce1c66650fa5b57
tebbaa-adnane <razorzeroday@gmail.com>
2022-06-02 16:55:40 +0100

    added React.

---
+++ my_app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from '../src/components/header';
+import '../src/assets/css'; 
+import '../src/assets/css/bootstrap-datetimepicker-standalone.css';
+import '../src/assets/css/bootstrap-datetimepicker.css';
+import '../src/assets/css/bootstrap-datetimepicker.min.css';
+import '../src/assets/css/bootstrap-select.css';
+import '../src/assets/css/bootstrap-select.min.css';
+import '../src/assets/css/bootstrap.css';
+import '../src/assets/css/bootstrap.min.css';
+import '../src/assets/css/costum-dev.css';
+import '../src/assets/css/default.css';
+import '../src/assets/css/documentation.css';
+import '../src/assets/css/font-awesome.min.css';
+import '../src/assets/css/monokai-sublime.css';
+import '../src/assets/css/owl.carousel.min.css';
+import '../src/assets/css/owl.theme.default.min.css';
+import '../src/assets/css/theme.css';
+import '../src/assets/css/theme.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ my_app/src/components/header.js
@@ -0,0 +1,69 @@
+import logo from '../assets/images/costum-images/Logo/Logo1v2.svg';
+import '../assets/css/bootstrap-datetimepicker-standalone.css';
+import '../assets/css/bootstrap-datetimepicker.css';
+import '../assets/css/bootstrap-datetimepicker.min.css';
+import '../assets/css/bootstrap-select.css';
+import '../assets/css/bootstrap-select.min.css';
+import '../assets/css/bootstrap.css';
+import '../assets/css/bootstrap.min.css';
+import '../assets/css/costum-dev.css';
+import '../assets/css/default.css';
+import '../assets/css/documentation.css';
+import '../assets/css/font-awesome.min.css';
+import '../assets/css/monokai-sublime.css';
+// import '../assets/css/owl.carousel.min.css';
+import '../assets/css/owl.theme.default.min.css';
+import '../assets/css/theme.css';
+import '../assets/css/theme.min.css';
+
+// import './App.css';
+
+function Header() {
+  return (
+    <header className="main-nav-container box">
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="main-nav">
+                    <a href="home/classNameic-agency.html" className="logo pull-left d-flex align-items-center">
+                        <img src={logo} alt=""/>
+                    </a>
+                    <nav className="menu pull-right">
+                        <ul className="menu-container list-unstyled d-flex align-items-center">
+                            <li className="menu-item open-sub-menu">
+                                <a className="nav-link submenu-icon" href="javascript:;">Home</a>
+                                <ul className="sub-menu list-unstyled box rounded-bottom">
+                                    <li className="sub-menu-item">
+                                        <a href="../blog/card-style.html" className="sub-menu-link">Blog</a>
+                                    </li>
+                                    <li className="sub-menu-item">
+                                        <a href="../blog/list-style.html" className="sub-menu-link">News Letter</a>
+                                    </li>
+                                    <li className="sub-menu-item">
+                                        <a href="../blog/profile.html" className="sub-menu-link">Connexion</a>
+                                    </li>
+                                    <li className="sub-menu-item">
+                                        <a href="../blog/single-article.html" className="sub-menu-link">Terms et
+                                            Conditions</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="menu-item d-none d-lg-block">
+                                <a className="nav-button transition-3d-hover"
+                                    href="https://themeforest.net/item/bodrum-modular-multi-purpose-html5-template/25649533"
+                                    target="_blank">Login</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <a className="menu-trigger">
+                        <span>Menu</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+  );
+}
+
+export default Header;
